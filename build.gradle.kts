801e3c0a8ce603b030d01e8254b82e55b3f35f73
actioninja <actioninja@gmail.com>
2019-04-14 18:07:40 -0700

    gradle update and build scanning

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     kotlin("jvm") version "1.3.20"
+    id("com.gradle.build-scan") version "2.2.1"
+}
+
+buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
 }
 
 group = "ninja.actio"
@@ -16,13 +22,16 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.uchuhimo", "kotlinx-bimap", "1.2")
 }
+
 repositories {
     mavenCentral()
 }
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
+
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
