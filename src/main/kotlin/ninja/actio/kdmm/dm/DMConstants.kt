ba200d1374c12342eebcde9ecb0e8b2edde596f6
actioninja <actioninja@gmail.com>
2019-04-27 21:45:40 -0700

    all direction constants

--- src/main/kotlin/ninja/actio/kdmm/dm/DMConstants.kt
+++ src/main/kotlin/ninja/actio/kdmm/dm/DMConstants.kt
@@ -1,6 +1,14 @@
 package ninja.actio.kdmm.dm
 
+
+//directions
 const val NORTH = 1
 const val SOUTH = 2
 const val EAST = 4
 const val WEST = 8
+const val NORTHEAST = 5
+const val NORTHWEST = 9
+const val SOUTHEAST = 6
+const val SOUTHWEST = 10
+const val UP = 16
+const val DOWN = 32
