8ab459d324b34abb08867d015587385acd5b1ec0
actioninja <actioninja@gmail.com>
2019-04-28 18:22:04 -0700

    start of object tree parser work

--- src/main/kotlin/ninja/actio/kdmm/dm/objecttree/ObjectTreeParser.kt
+++ src/main/kotlin/ninja/actio/kdmm/dm/objecttree/ObjectTreeParser.kt
@@ -1,2 +1,83 @@
 package ninja.actio.kdmm.dm.objecttree
 
+import ninja.actio.kdmm.dm.getFileInternal
+import ninja.actio.kdmm.dm.stripComments
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.nio.file.Path
+
+class ObjectTreeParser(val objectTree: ObjectTree = ObjectTree()) {
+    companion object {
+        //Taken from FastDMM
+        private val QUOTES_PATTERN = Regex("^\"(.*)\"$")
+        private val DEFINE_PATTERN = Regex("#define +([\\d\\w]+) +(.+)")
+        private val UNDEF_PATTERN = Regex("#undef[ \\t]*([\\d\\w]+)")
+        private val MACRO_PATTERN = Regex("(?<![\\d\\w\"])\\w+(?![\\d\\w\"])")
+    }
+
+    private var isCommenting = false
+    private var isMultilineString = false
+    private var multilineStringDepth = 0
+    private var parenthesisDepth = 0
+    private var stringDepth = 0
+    private var stringExpDepth = 0
+
+    private val macros = mutableMapOf<String, String>()
+
+    fun parseDME(file: File) {
+        subParse(getFileInternal("stddef.dm"))
+    }
+
+    fun parseFile(file: File) {
+        subParse(file.inputStream())
+    }
+
+    fun parse(stream: InputStream) {
+        val lines = cleanAndListize(stream)
+    }
+
+    fun subParse(stream: InputStream) {
+        val parser = ObjectTreeParser(objectTree)
+        parser.macros.putAll(macros)
+        parser.parse(stream)
+    }
+
+    //Turns spaces into tabs, strips comments, and single lines multilines, clears blank lines, then returns the file as a list instead of a stream
+    fun cleanAndListize(stream: InputStream): List<String> {
+        val reader = BufferedReader(InputStreamReader(stream))
+        val lines = mutableListOf<String>()
+        val runOn = StringBuilder()
+        lateinit var line: String
+        var firstMultiline = true
+
+        reader.forEachLine {
+            line = it
+            line = stripComments(line)
+            line = line.replace('\t', ' ')
+            if(line.isNotBlank()) {
+                if(line.endsWith('\\')) {
+                    line = line.removeSuffix("\\")
+                    if(firstMultiline) {
+                        firstMultiline = false
+                    } else {
+                        line = line.trimStart()
+                    }
+                    runOn.append(line)
+
+                } else {
+                    if(!firstMultiline)
+                        line = line.trimStart()
+                    runOn.append(line)
+                    line = runOn.toString()
+                    runOn.clear()
+                    firstMultiline = true
+                    lines.add(line)
+                }
+            }
+        }
+
+        return lines.toList()
+    }
+}
--- src/test/kotlin/ninja/actio/kdmm/test/objecttree/ObjectTreeParserTests.kt
+++ src/test/kotlin/ninja/actio/kdmm/test/objecttree/ObjectTreeParserTests.kt
@@ -1,2 +1,37 @@
 package ninja.actio.kdmm.test.objecttree
 
+import ninja.actio.kdmm.dm.objecttree.ObjectTreeParser
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ObjectTreeParserTests {
+
+    val classLoader = javaClass.classLoader
+    val toBeCleanedStream = classLoader.getResourceAsStream("clean_and_listize_test.txt")
+    val genericParser = ObjectTreeParser()
+
+    val expectedCleanedAndListized = listOf(
+        "/obj/effect/abstract/ripple",
+        " name = \"hyperspace ripple\"",
+        " desc = \"Something is coming through hyperspace, you can see the visual disturbances. It's probably best not to be on top of these when whatever is tunneling comes through.\"",
+        " icon = 'icons/effects/effects.dmi'",
+        " icon_state = \"medi_holo\"",
+        " anchored = TRUE",
+        " density = FALSE",
+        " layer = RIPPLE_LAYER",
+        " mouse_opacity = MOUSE_OPACITY_ICON",
+        " alpha = 0",
+        "/obj/effect/abstract/ripple/Initialize(mapload, time_left)",
+        " . = ..()",
+        " animate(src, alpha=255, time=time_left)",
+        " addtimer(CALLBACK(src, .proc/stop_animation), 8, TIMER_CLIENT_TIME)",
+        "/obj/effect/abstract/ripple/proc/stop_animation()",
+        " icon_state = \"medi_holo_no_anim\""
+    )
+
+    @Test
+    fun `Clean and Listize Test`() {
+        val cleaned = genericParser.cleanAndListize(toBeCleanedStream)
+        assertEquals(expectedCleanedAndListized, cleaned)
+    }
+}
