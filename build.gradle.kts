6ee128abe3a678e18b9667ceda45713a31056cd5
actioninja <gardad91@gmail.com>
2019-05-05 02:58:31 -0700

    adds logback to the build file so we have proper logging actually working

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,14 +45,18 @@ dependencies {
     implementation("com.uchuhimo", "kotlinx-bimap", "1.2")
     implementation("io.github.microutils", "kotlin-logging", "1.6.24")
     implementation("no.tornado", "tornadofx", "2.0.0-SNAPSHOT")
+    implementation("ch.qos.logback", "logback-core", "1.2.3")
+    implementation("ch.qos.logback", "logback-classic", "1.2.3")
+    implementation("ch.qos.logback", "logback-access", "1.2.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.3.1")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.2.70")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.3.31")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
 }
 
 repositories {
     mavenCentral()
+    jcenter()
     maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
